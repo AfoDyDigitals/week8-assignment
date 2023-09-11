d070e38e41f8b009df517653db1363fb79adeb6a
AfoDy <afodydigitals@gmail.com>
2023-09-10 23:12:29 -0700

    create a gameRule function

--- Resources/Javascript/script.js
+++ Resources/Javascript/script.js
@@ -14,16 +14,54 @@ function getUserChoice(){
         ).toLowerCase();
     
     if (playerInput == "rock") {
-        console.log("You selected rock");
+        console.log("You picked rock");
         return "Rock";
     }   else if (playerInput == "paper") {
-        console.log("You selected paper");
+        console.log("You picked paper");
         return "Paper";
     }   else if (playerInput == "scissors") {
         console.log("You picked scissors");
-        return "scissors";
-    } else{
-        console.log("Please select either Rock, Paper, or Scissors");
+        return "Scissors";
+    } 
+    else{
+        console.log("Invalid selection");
     }
 };
 // getUserChoice();
+
+function gameRule(){
+    let playerSelection = getUserChoice();
+    let computerSelection = getComputerChoice();
+
+    if (playerSelection === computerSelection){
+        console.log("It's a tie. Retry your luck!");
+        return "tie";
+      }  else if (playerSelection === "Rock"){
+            if (computerSelection === "Paper"){
+                console.log ("Aww... paper covers rock, computer wins!");
+                return "lose";
+            }   else if (computerSelection === "Scissors"){
+                console.log ("Rock crushes scissors, well done, you win!");
+                return "win";
+            }
+        } else if (playerSelection === "Paper"){
+            if (computerSelection === "Rock"){
+                console.log ("Paper covers rock, well done, you win!");
+                return "win";
+            }  else if (computerSelection === "Scissors"){
+            console.log("Aww... scissors cuts paper, computer wins!");
+            return "lose";
+            } 
+        } else if (playerSelection === "Scissors"){
+                if (computerSelection === "Paper"){
+                console.log ("Scissors cuts paper, well done, you win!");
+                return "win";
+        } else if(computerSelection === "Rock"){
+            console.log ("Aww... rock crushes scissors, computer wins!");
+            return "lose";
+        }
+        } else {
+        console.log("Please, enter a valid selection among Rock, Paper, and Scissors");
+    }
+    }
+gameRule();
